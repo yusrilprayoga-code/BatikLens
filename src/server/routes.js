895e81d7c1148fb1a1da67ae4f27818705bea76b
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-05-26 19:21:50 +0700

    Update login and register

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,37 +2,63 @@ const {
   registerHandler,
   loginHandler,
   // postPredictHandler,
-} = require('./handler');
+} = require("./handler");
 
 const routes = [
   {
-    method: 'POST',
-    path: '/register',
+    method: "POST",
+    path: "/register",
     handler: registerHandler,
     options: {
       auth: false,
       payload: {
-        allow: 'application/json',
+        allow: "application/json",
       },
     },
   },
   {
-    method: 'POST',
-    path: '/login',
+    method: "POST",
+    path: "/login",
     handler: loginHandler,
     options: {
       auth: false,
     },
   },
   {
-    method: 'GET',
-    path: '/protected',
+    method: "GET",
+    path: "/protected",
     handler: (request, h) => {
-      return h.response({
-        status: 'success',
-        message: 'You have accessed a protected route!',
-        user: request.auth.credentials.user,
-      }).code(200);
+      return h
+        .response({
+          status: "success",
+          message: "You have accessed a protected route!",
+          user: request.auth.credentials.user,
+        })
+        .code(200);
+    },
+    options: {
+      auth: 'jwt',
+    }
+  },
+  {
+    method: "GET",
+    path: "/set-cookie",
+    handler: async (request, h) => {
+      h.state("auth", "your_cookie_value", {
+        ttl: 24 * 60 * 60 * 1000,
+        isSecure: false,
+        path: "/",
+      });
+
+      return h
+        .response({
+          status: "success",
+          message: "Cookie set successfully",
+        })
+        .code(200);
+    },
+    options: {
+      auth: 'jwt',
     },
   },
   // {
--- src/server/server.js
+++ src/server/server.js
@@ -1,82 +1,75 @@
-require('dotenv').config();
- 
-const Hapi = require('@hapi/hapi');
-const routes = require('../server/routes');
-// const loadModel = require('../services/loadModel');
-const InputError = require('../exceptions/InputError');
-const Jwt = require('@hapi/jwt');
- 
+const Hapi = require("@hapi/hapi");
+const routes = require("../server/routes");
+const InputError = require("../exceptions/InputError");
+const Jwt = require("@hapi/jwt");
+const Cookie = require("@hapi/cookie");
+require("dotenv").config();
+
 (async () => {
-    const server = Hapi.server({
-        port: process.env.PORT,
-        host: 'localhost',
-        routes: {
-            cors: {
-              origin: ['*'],
-            },
-        },
-    });
- 
-    // const model = await loadModel();
-    // server.app.model = model;
- 
-    server.route(routes);
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
 
-    server.ext('onPreResponse', function (request, h) {
-        const response = request.response;
- 
-        if (response instanceof InputError) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: `${response.message}`
-            })
-            newResponse.code(response.statusCode)
-            return newResponse;
-        }
+  // const model = await loadModel();
+  // server.app.model = model;
 
-        if (response.isBoom && response.output.statusCode === 413) {
-            const newResponse = h.response({
-                status: 'fail',
-                message: 'Payload content length greater than maximum allowed: 1000000'
-            })
-            newResponse.code(413)
-            return newResponse;
-        }
- 
-        return h.continue;
-    });
+  server.ext("onPreResponse", function (request, h) {
+    const response = request.response;
 
-    //make a register
-    await server.register(Jwt);
+    if (response instanceof InputError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: `${response.message}`,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
 
-    //define the strategy
-    server.auth.strategy('jwt', 'jwt', {
-        keys: process.env.JWT_SECRET,
-        verify: {
-            aud: false,
-            iss: false,
-            sub: false,
-            nbf: true,
-            exp: true,
-            maxAgeSec: 14400
-        },
-        validate: (artifacts, request, h) => {
+    if (response.isBoom && response.output.statusCode === 413) {
+      const newResponse = h.response({
+        status: "fail",
+        message: "Payload content length greater than maximum allowed: 1000000",
+      });
+      newResponse.code(413);
+      return newResponse;
+    }
 
-            if (!artifacts.decoded.payload.user) {
-                return {
-                    isValid: false
-                };
-            }
-            
-            return {
-                isValid: true,
-                credentials: { user: artifacts.decoded.payload.user }
-            };
-        }
-    });
+    return h.continue;
+  });
 
-    server.auth.default('jwt');
+  await server.register([Jwt, Cookie]);
 
-    await server.start();
-    console.log(`Server start at: ${server.info.uri}`);
+  server.auth.strategy("jwt", "jwt", {
+    keys: process.env.JWT_SECRET,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      nbf: true,
+      exp: true,
+      maxAgeSec: 14400,
+    },
+    validate: (artifacts, request, h) => {
+      if (!artifacts.decoded.payload.user) {
+        return { isValid: false };
+      }
+      return {
+        isValid: true,
+        credentials: { user: artifacts.decoded.payload.user },
+      };
+    },
+  });
+
+  server.auth.default("jwt");
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server started at: ${server.info.uri}`);
 })();
