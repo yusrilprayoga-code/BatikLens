0d4c9d472641432162744163244cd5d3de61e646
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-06-04 00:34:32 +0700

    Update Model to Batik Model

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,10 +1,10 @@
 const tf = require('@tensorflow/tfjs-node');
 
 async function loadModel() {
-    return tf.loadLayersModel(
-        `file://${process.cwd()}/src/batikmodel/model.json`
-    );
-    // return tf.loadGraphModel(process.env.MODEL_URL);
+    // return tf.loadLayersModel(
+    //     `file://${process.cwd()}/src/batikmodel/model.json`
+    // );
+    return tf.loadLayersModel(process.env.MODEL_URL);
 }
  
 module.exports = loadModel;
